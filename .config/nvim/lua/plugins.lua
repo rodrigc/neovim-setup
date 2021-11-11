913299d58a23c54136ae5602f7fa5ca6dcbb17c0
Craig Rodrigues <rodrigc@crodrigues.org>
2021-11-10 17:19:27 -0800

    Initial commit of neovim 0.5.1 config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,83 @@
+-- This file can be loaded by calling `require('plugins')` from your init.lua
+
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- monokai colorscheme
+  use 'tanvirtin/monokai.nvim'
+
+  -- nvim-treesitter
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- Asynchronous Linter (ale)
+  use 'dense-analysis/ale'
+
+  -- ncm2 completion manager
+  use 'ncm2/ncm2'
+
+  -- fuzzy finder
+  use 'junegunn/fzf'
+  use 'junegunn/fzf.vim'
+
+  -- Semantic language support
+  -- Collection of common configurations for the Nvim LSP client
+  use 'neovim/nvim-lspconfig'
+  -- Extensions to built-in LSP, for example, providing type inlay hints
+  use 'nvim-lua/lsp_extensions.nvim'
+  
+  -- Autocompletion framework
+  use 'hrsh7th/nvim-cmp'
+  -- cmp LSP completion
+  use 'hrsh7th/cmp-nvim-lsp'
+  -- cmp Snippet completion
+  use 'hrsh7th/cmp-vsnip'
+  -- cmp Path completion
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
+  -- See hrsh7th other plugins for more great completion sources!
+  -- Snippet engine
+  use 'hrsh7th/vim-vsnip'
+
+  -- Show function signature when you type
+  use "ray-x/lsp_signature.nvim"
+
+  -- Syntactic language support
+  -- Rust
+  use 'rust-lang/rust.vim'
+  -- To enable more of the features of rust-analyzer, such as inlay hints and more!
+  -- See: https://sharksforarms.dev/posts/neovim-rust/
+  use 'simrat39/rust-tools.nvim'
+  -- Go
+  use 'fatih/vim-go'
+
+  -- Optional dependencies
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-media-files.nvim'
+
+  -- Debugging (needs plenary from above as well) 
+  use 'mfussenegger/nvim-dap'
+
+  -- GUI enhancements 
+  use 'itchyny/lightline.vim'
+
+   -- Tree browser
+   use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function() require'nvim-tree'.setup {} end
+   }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # neovim-setup
 Config files for neovim
+
+## Instructions
+1.  Install neovim v0.5.1
+2.  Copy the .config/nvim directory from this repository to $HOME/.config/nvim
+    directory on your system.
+
+## Further resources
+* [Desktop and editor setup for Rust development](https://www.youtube.com/watch?v=ycMiMDHopNc)
+* [Neovim and Rust](https://sharksforarms.dev/posts/neovim-rust/)
+* [Neovim - Convert init.vim to init.lua (Live)][https://www.youtube.com/watch?v=BoDU6QOmEOY]
+
