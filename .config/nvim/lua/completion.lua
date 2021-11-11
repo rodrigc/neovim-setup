1f9ac79ee10fb21814b57c0542013d15c2b19d12
Craig Rodrigues <rodrigc@crodrigues.org>
2021-11-11 11:26:55 -0800

    Minor update

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -10,7 +10,7 @@ cmp.setup({
       vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
     end,
   },
   mapping = {
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -72,7 +72,7 @@ return require('packer').startup(function()
    use {
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
-    config = function() require'nvim-tree'.setup {} end
+    config = function() require('nvim-tree').setup({}) end
    }
 
   -- Automatically set up your configuration after cloning packer.nvim
