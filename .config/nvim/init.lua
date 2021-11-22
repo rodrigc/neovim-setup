dfda7c19dc1f628a4a4ff4b86737f15001ccf3a0
Craig Rodrigues <rodrigc@crodrigues.org>
2021-11-22 11:45:42 -0800

    Separate telescope config to separate file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,4 @@ require('completion')
 require('lsp')
 require('rust') 
 
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+require('config/telescope')
