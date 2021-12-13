f0991e90dbd50e05303219029cb1dd86cd1ee00f
Craig Rodrigues <rodrigc@crodrigues.org>
2021-12-12 20:08:46 -0800

    Map cursor keys to moving between windows
    Inspired by: https://www.youtube.com/watch?v=I0PrxH53Rfc

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -5,3 +5,14 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, s
 vim.api.nvim_set_keymap('n', 'fb',  [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'ff',  [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>tegf',  [[<Cmd>lua require('telescope.builtin').git_files()<CR>]], { noremap = true, silent = true })
+
+-- Move between Vim Windows
+vim.api.nvim_set_keymap('n', '<up>',  "<C-W><up>", { noremap = false })
+vim.api.nvim_set_keymap('n', '<down>',  "<C-W><down>", { noremap = false })
+vim.api.nvim_set_keymap('n', '<left>',  "<C-W><left>", { noremap = false })
+vim.api.nvim_set_keymap('n', '<right>',  "<C-W><right>", { noremap = false })
+-- Resize Vim Windows
+vim.api.nvim_set_keymap('n', '<S-up>',  ":resize -2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-down>',  ":resize +2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-left>',  ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-right>',  ":vertical resize +2<CR>", { noremap = true, silent = true })
