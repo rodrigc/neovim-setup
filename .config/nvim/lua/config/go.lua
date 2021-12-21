eb18307410c8e6528181cd2505d0231934b4993e
Craig Rodrigues <rodrigc@crodrigues.org>
2021-12-21 01:19:03 -0800

    Use lsp setup inside go.nvim

--- .config/nvim/lua/config/go.lua
+++ .config/nvim/lua/config/go.lua
@@ -2,15 +2,15 @@ local lspconfig = require('lspconfig')
 local lsp = require('lsp')
 
 
-lspconfig.gopls.setup {
-  capabilities = lsp.capabilities,
-  on_attach = lsp.on_attach,
-  flags = {
-    debounce_text_changes = 150,
-  },
-}
+-- lspconfig.gopls.setup {
+--   capabilities = lsp.capabilities,
+--   on_attach = lsp.on_attach,
+--   flags = {
+--     debounce_text_changes = 150,
+--   },
+-- }
 
-require('go').setup()
+require('go').setup({lsp_cfg = true, lsp_on_attach = lsp.on_attach})
 
 -- Run goimport on save
 vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
