fa805ad6648fe2a78605afc3672388a8e0dd41aa
Craig Rodrigues <rodrigc@crodrigues.org>
2022-06-27 19:13:54 -0700

    Update keymaps to use vim.keymap.set

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -1,21 +1,21 @@
 -- NvimTree
-vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-n>', function() require('nvim-tree').toggle(true) end, { silent = true })
 
 -- Telescope
-vim.api.nvim_set_keymap('n', 'fb',  [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'ff',  [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>tegf',  [[<Cmd>lua require('telescope.builtin').git_files()<CR>]], { noremap = true, silent = true })
+vim.keymap.set('n', 'fb',  function() require('telescope.builtin').buffers() end, { silent = true })
+vim.keymap.set('n', 'ff',  function() require('telescope.builtin').find_files() end, { silent = true })
+vim.keymap.set('n', '<Leader>tegf',  function() require('telescope.builtin').git_files() end, { silent = true })
 
 -- Move between Vim Windows
-vim.api.nvim_set_keymap('n', '<up>',  "<C-W><up>", { noremap = false })
-vim.api.nvim_set_keymap('n', '<down>',  "<C-W><down>", { noremap = false })
-vim.api.nvim_set_keymap('n', '<left>',  "<C-W><left>", { noremap = false })
-vim.api.nvim_set_keymap('n', '<right>',  "<C-W><right>", { noremap = false })
+vim.keymap.set('n', '<up>',  "<C-W><up>", { noremap = false })
+vim.keymap.set('n', '<down>',  "<C-W><down>", { noremap = false })
+vim.keymap.set('n', '<left>',  "<C-W><left>", { noremap = false })
+vim.keymap.set('n', '<right>',  "<C-W><right>", { noremap = false })
 -- Resize Vim Windows
-vim.api.nvim_set_keymap('n', '<S-up>',  ":resize -2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-down>',  ":resize +2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-left>',  ":vertical resize -2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-right>',  ":vertical resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<S-up>',  ":resize -2<CR>", { silent = true })
+vim.keymap.set('n', '<S-down>',  ":resize +2<CR>", { silent = true })
+vim.keymap.set('n', '<S-left>',  ":vertical resize -2<CR>", { silent = true })
+vim.keymap.set('n', '<S-right>',  ":vertical resize +2<CR>", { silent = true })
 
 -- Open terminal inside new window
-vim.api.nvim_set_keymap('n', '<Leader>tt',  ":vnew|terminal<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>tt',  ":vnew|terminal<CR>", { silent = true })
--- .config/nvim/lua/config/ruby.lua
+++ .config/nvim/lua/config/ruby.lua
@@ -5,33 +5,28 @@ local nvim_lsp = require('lspconfig')
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
   --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder)
+  vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+  vim.keymap.set('n', '<space>e', vim.lsp.diagnostic.show_line_diagnostics)
+  vim.keymap.set('n', '[d', vim.lsp.diagnostic.goto_prev)
+  vim.keymap.set('n', ']d', vim.lsp.diagnostic.goto_next)
+  vim.keymap.set('n', '<space>q', vim.lsp.diagnostic.set_loclist)
+  vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting)
 
 end
 
