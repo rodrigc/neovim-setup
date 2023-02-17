b243d528972f82ce5fc91e717ac9793010dd9e39
Craig Rodrigues <rodrigc@crodrigues.org>
2023-02-17 11:18:05 -0800

    Switch to ruff_lsp for Python

--- .config/nvim/lua/config/python.lua
+++ .config/nvim/lua/config/python.lua
@@ -1,6 +1,9 @@
 local nvim_lsp = require('lspconfig')
 
-nvim_lsp["pyright"].setup {
+-- Configure `ruff-lsp`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- For the default config, along with instructions on how to customize the settings
+nvim_lsp["ruff_lsp"].setup {
     on_attach = require('config/lsp').on_attach,
     flags = {
       debounce_text_changes = 150,
