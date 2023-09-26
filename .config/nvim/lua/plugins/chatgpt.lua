3865627b5fa70062fba893cb1388a82196cf2330
Craig Rodrigues <rodrigc@crodrigues.org>
2023-09-26 07:55:16 -0700

    Add some plugins for AI

---
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,14 @@
+return {
+  'jackMort/ChatGPT.nvim',
+  enabled = false,
+  config = function()
+    require("chatgpt").setup({
+      -- optional configuration
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim"
+  }
+}
