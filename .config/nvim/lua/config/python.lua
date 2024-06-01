b6806217201faaf1c8b96f07cdf5dd1abe64e83f
Craig Rodrigues <rodrigc@crodrigues.org>
2024-06-01 01:25:53 -0700

    Update Python setup

--- .config/nvim/lua/config/python.lua
+++ .config/nvim/lua/config/python.lua
@@ -1,11 +1,34 @@
 local nvim_lsp = require('lspconfig')
 
+local on_attach = function(client, bufnr)
+  if client.name == 'ruff_lsp' then
+    -- Disable hover in favor of Pyright
+    client.server_capabilities.hoverProvider = false
+  end
+end
+
 -- Configure `ruff-lsp`.
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
 -- For the default config, along with instructions on how to customize the settings
-nvim_lsp["ruff_lsp"].setup {
-    on_attach = require('config/lsp').on_attach,
+nvim_lsp.ruff_lsp.setup {
+    on_attach = on_attach,
     flags = {
       debounce_text_changes = 150,
     }
+}
+
+nvim_lsp.pyright.setup {
+  on_attach = on_attach,
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
 }
