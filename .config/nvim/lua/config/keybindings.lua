6bd68e56e23ad492a85c629d7dd91462ca740f82
Craig Rodrigues <rodrigc@crodrigues.org>
2024-09-02 19:55:25 -0700

    Add descriptions to keybindings

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -13,15 +13,15 @@ vim.keymap.set('n', '<Leader>tegf', function() require('telescope.builtin').git_
 	{ silent = true, desc = 'Telescope git files' })
 
 -- Move between Vim Windows
-vim.keymap.set('n', '<up>', "<C-W><up>", { noremap = false })
-vim.keymap.set('n', '<down>', "<C-W><down>", { noremap = false })
-vim.keymap.set('n', '<left>', "<C-W><left>", { noremap = false })
-vim.keymap.set('n', '<right>', "<C-W><right>", { noremap = false })
+vim.keymap.set('n', '<up>', "<C-W><up>", { noremap = false, desc = 'Go to the up window' })
+vim.keymap.set('n', '<down>', "<C-W><down>", { noremap = false, desc = 'Go to the down window' })
+vim.keymap.set('n', '<left>', "<C-W><left>", { noremap = false, desc = 'Go to the left window' })
+vim.keymap.set('n', '<right>', "<C-W><right>", { noremap = false, desc = 'Go to the right window' })
 -- Resize Vim Windows
-vim.keymap.set('n', '<S-up>', ":resize -2<CR>", { silent = true })
-vim.keymap.set('n', '<S-down>', ":resize +2<CR>", { silent = true })
-vim.keymap.set('n', '<S-left>', ":vertical resize -2<CR>", { silent = true })
-vim.keymap.set('n', '<S-right>', ":vertical resize +2<CR>", { silent = true })
+vim.keymap.set('n', '<S-up>', ":resize -2<CR>", { silent = true, desc = 'resize horizontal -2' })
+vim.keymap.set('n', '<S-down>', ":resize +2<CR>", { silent = true, desc = 'resize horizontal +2' })
+vim.keymap.set('n', '<S-left>', ":vertical resize -2<CR>", { silent = true, desc = 'resize vertical -2' })
+vim.keymap.set('n', '<S-right>', ":vertical resize +2<CR>", { silent = true, desc = 'resize vertical +2' })
 
 -- Open terminal inside new window
 vim.keymap.set('n', '<Leader>tt', ":vnew|terminal<CR>", { silent = true, desc = 'open new terminal' })
