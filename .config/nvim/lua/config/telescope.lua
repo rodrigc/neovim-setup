81e766523b075df598d06e227da0ee630fe11c20
Craig Rodrigues <rodrigc@crodrigues.org>
2024-10-22 10:32:47 -0700

    Add telescope config

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,3 +1,21 @@
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
+
+-- https://www.reddit.com/r/neovim/comments/qspemc/close_buffers_with_telescope/
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      n = {
+    	  ['<c-d>'] = require('telescope.actions').delete_buffer
+      }, -- n
+      i = {
+        ["<C-h>"] = "which_key",
+        ['<c-d>'] = require('telescope.actions').delete_buffer
+      } -- i
+    } -- mappings
+  }, -- defaults
+...
+} -- telescope setup
