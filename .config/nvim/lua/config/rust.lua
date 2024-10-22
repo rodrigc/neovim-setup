6450252c9a233aaafde103366a1bdb0b0d2da656
Craig Rodrigues <rodrigc@crodrigues.org>
2024-10-22 10:33:20 -0700

    Update rust config

--- .config/nvim/lua/config/rust.lua
+++ .config/nvim/lua/config/rust.lua
@@ -8,7 +8,7 @@
 -- menuone: popup even when there's only one match
 -- noinsert: Do not insert text until a selection is made
 -- noselect: Do not select, force user to select one from the menu
-vim.opt.completeopt = {"menuone","noinsert","noselect"}
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
 
 -- Avoid showing extra messages when using completion
 vim.opt.shortmess:append('c')
@@ -25,39 +25,51 @@ local codelldb_path = extension_path .. 'adapter/codelldb'
 local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
 
 vim.g.rustaceanlocal = {
-    tools = { -- rust-tools options
-    },
+	tools = { -- rust-tools options
+	},
 
-    -- debugging stuff
-    dap = {
-        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
-    },
+	-- debugging stuff
+	dap = {
+		adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+	},
 
-    -- all the opts to send to nvim-lspconfig
-    -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = {
-        capabilities = lsp.capabilities,
+	-- all the opts to send to nvim-lspconfig
+	-- these override the defaults set by rust-tools.nvim
+	-- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+	server = {
+		capabilities = lsp.capabilities,
 
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                cargo = {
-                    features = "all",
-                },
-            -- Add the following line to enable rustfmt on save
-                ["rust-analyzer.rustfmt.overrideCommand"] = { "rustfmt", "--edition", "2021", "--emit=stdout" },
-            }
-        }
-    },
+		settings = {
+			-- to enable rust-analyzer settings visit:
+			-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+			["rust-analyzer"] = {
+				cargo = {
+					features = "all",
+				},
+				-- Add the following line to enable rustfmt on save
+				["rust-analyzer.rustfmt.overrideCommand"] = { "rustfmt", "--edition", "2021", "--emit=stdout" },
+
+				-- Taken from https://book.leptos.dev/getting_started/leptos_dx.html
+				-- Other Settings ...
+				procMacro = {
+					ignored = {
+						leptos_macro = {
+							-- optional: --
+							-- "component",
+							"server",
+						},
+					},
+				},
+			}
+		}
+	},
 }
 
-local format_sync_grp = vim.api.nvim_create_augroup("Format", { clear = false})
+local format_sync_grp = vim.api.nvim_create_augroup("Format", { clear = false })
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.rs",
-  callback = function()
-   vim.lsp.buf.format({ timeout_ms = 200 })
-  end,
-  group = format_sync_grp,
+	pattern = "*.rs",
+	callback = function()
+		vim.lsp.buf.format({ timeout_ms = 200 })
+	end,
+	group = format_sync_grp,
 })
