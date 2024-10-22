f04f441efa1e9aee5293f319ae37501ed1447b6b
Craig Rodrigues <rodrigc@crodrigues.org>
2024-10-22 10:33:06 -0700

    Handle renaming of ts lsp server

--- .config/nvim/lua/config/javascript.lua
+++ .config/nvim/lua/config/javascript.lua
@@ -1,7 +1,7 @@
 local nvim_lsp = require('lspconfig')
 
 -- https://github.com/craftzdog/dotfiles-public/blob/105681a36498d9514e5221f2f8f6ab08e2d2c10c/.config/nvim/plugin/lspconfig.lua#L75
-nvim_lsp.tsserver.setup {
+nvim_lsp.ts_ls.setup {
     filetypes = {"typescript", "typescriptreact", "typescript.tsx"},
     cmd = { "typescript-language-server", "--stdio" },
     flags = {
