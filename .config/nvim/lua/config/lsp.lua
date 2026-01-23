7aecfa16395a80df5b942b12faa5030c84485220
Craig Rodrigues <rodrigc@crodrigues.org>
2026-01-23 00:47:57 -0800

    Add descriptions to lsp keymaps

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -3,6 +3,14 @@
 -- Taken from: https://github.com/neovim/nvim-lspconfig/blob/master/README.md#keybindings-and-completion
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
+		local lsp_keymap_set = function(mode, lhs, rhs, desc)
+			vim.keymap.set(mode, lhs, rhs, {
+				buffer = ev.buf,
+				silent = true,
+				desc = desc,
+			})
+		end
+
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		if client == nil then
 			return
@@ -19,25 +27,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
 
 		-- Mappings using new API style
-		local opts = { buffer = ev.buf, silent = true }
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wl', function()
+		lsp_keymap_set('n', 'gD', vim.lsp.buf.declaration, 'LSP: Go to declaration')
+		lsp_keymap_set('n', 'gd', vim.lsp.buf.definition, 'LSP: Go to definition')
+		lsp_keymap_set('n', 'gi', vim.lsp.buf.implementation, 'LSP: Go to implementation')
+		lsp_keymap_set('n', 'gr', vim.lsp.buf.references, 'LSP: List references')
+		lsp_keymap_set('n', 'K', vim.lsp.buf.hover, 'LSP: Hover documentation')
+		lsp_keymap_set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, 'LSP: Add workspace folder')
+		lsp_keymap_set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, 'LSP: Remove workspace folder')
+		lsp_keymap_set('n', '<space>wl', function()
 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-		vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-		vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1}) end, opts)
-		vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1}) end, opts)
-		vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-		vim.keymap.set('n', '<space>f', vim.lsp.buf.format, opts)
+		end, 'LSP: List workspace folders')
+		lsp_keymap_set('n', '<space>D', vim.lsp.buf.type_definition, 'LSP: Go to type definition')
+		lsp_keymap_set('n', '<space>rn', vim.lsp.buf.rename, 'LSP: Rename symbol')
+		lsp_keymap_set('n', '<space>ca', vim.lsp.buf.code_action, 'LSP: Code action')
+		lsp_keymap_set('n', '<space>e', vim.diagnostic.open_float, 'Show diagnostic message')
+		lsp_keymap_set('n', '[d', function() vim.diagnostic.jump({ count = -1 }) end, 'Previous diagnostic')
+		lsp_keymap_set('n', ']d', function() vim.diagnostic.jump({ count = 1 }) end, 'Next diagnostic')
+		lsp_keymap_set('n', '<space>q', vim.diagnostic.setloclist, 'Set loclist')
+		lsp_keymap_set('n', '<space>f', vim.lsp.buf.format, 'Format buffer')
 
 		-- Get signatures (and _only_ signatures) when in argument lists
 		require("lsp_signature").on_attach({
